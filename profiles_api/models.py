b1854661b7a703e58ebb17902c2fd15457257098
Robin <duckfeces@gmail.com>
2022-02-18 14:12:41 +0800

    Added custom user profile model, manager and migration

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -9,47 +9,47 @@ class UserProfileManager(BaseUserManager):
 
     def create_user(self, email, name, password=None):
         """Create a new user profile"""
-            if not email:
-                raise ValueError('User must have an email address')
+        if not email:
+            raise ValueError('Users must have an email address')
 
-                email = self.normalize_email(email)
-                user = self.model(email=email, name=name)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
 
-                user.set_password(password)
-                user.save(using=self._db)
+        user.set_password(password)
+        user.save(using=self._db)
 
-            return user
+        return user
 
-        def create_superuser(self, email, name, password):
-            """Create and save a new superuser with given details"""
-            user = self.create_user(email, name, password)
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-            user.is_superuser = true
-            user.is_staff = true
-            user.save(using=self._db)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-            return user
+        return user
 
 class UserProfile (AbstractBaseUser, PermissionsMixin):
-"""Database model for users in the system"""
-email = models.EmailField(max_length=255, unique=true)
-name = models.CharField(max_length=255)
-is_active = models.BooleanField(default=True)
-is_staff = models.BooleanField(default=False)
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-objects = UserProfileManage()
+    objects = UserProfileManager()
 
-USERNAME_FIELD = 'email'
-REQUIRED_FIELD = ['name']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = ['name']
 
-def get_full_name(self):
-    """Retrieve full name of user"""
-    return self.name
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
 
-def get_short_name(self):
-    """Retrieve short name of user"""
-    return self.name
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
 
-def __str__(self):
-"""Return string representation of our user"""
-return self.email
+    def __str__(self):
+        """Return string representation of our user"""
+        return self.email
