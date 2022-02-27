25912c354f996350b39690eb5deb478e8ce7cd08
Robin <duckfeces@gmail.com>
2022-02-27 08:19:16 +0800

    TESTING

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = 'om13ylqq^(pw+!y!3$*u35e=8qdn@^tp&@^+#7j-mv_w^mvj!m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = ['ec2-18-234-165-250.compute-1.amazonaws.com',
- '127.0.0.1'
+ALLOWED_HOSTS = [
+    'ec2-18-234-165-250.compute-1.amazonaws.com',
+    '127.0.0.1'
  ]
 
 
